f245fd9891eb10628d4efb6c9e3078363893a85a
Daniel <danieldreid@gmail.com>
2023-08-28 18:08:06 +0800

    move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,2 +1,25 @@
 export const cart = [];
 
+export function addToCart(productId) {
+
+  let cartQuantitySelection = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+
+  let matchingItem;
+  
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    } 
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1 * cartQuantitySelection;
+  } else {
+
+    cart.push({
+      productId: productId, 
+      quantity: 1 * cartQuantitySelection,
+    });
+  }
+
+};
